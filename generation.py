c08e7d14918bb8a6c894a728cd11dedf0d1c72d3
bendich69 <bendich48@gmail.com>
2026-03-20 17:12:12 +0300

    Update generation.py

--- generation.py
+++ generation.py
@@ -1,2 +1,8 @@
-# TODO: Напиши функцию geration()
+import pandas as pd
+def calculate_generation(weather_data):
+  results = []
+  for _, row in weather_data.iterrows(weather_data):
+      results.append({'Тики': row['Время'] , 'СЭС': row['Облачность']*0.1-4.1, 'Ветряк': row['Ветер']*0.70+2.3})
+  calculate_generation = pd.DataFrame(results)
+  print(calculate_generation.to_string(index=False)) 
 pass
